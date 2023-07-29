2a0aaedf49e77a486717185f659b15e2ad620b9e
strurao <strurao@gmail.com>
2023-07-29 13:53:40 +0900

    연결리스트

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -1,7 +1,14 @@
 ﻿#include <iostream>
+#include "List.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	List list;
+
+	list.AddAtTail(1);
+	list.AddAtTail(2);
+	list.AddAtTail(3);
+
+	list.Print();
 }
 
