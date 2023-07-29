e0d33fff321991c102327ee8f64cfcbcdf0c1fa0
strurao <strurao@gmail.com>
2023-07-29 16:01:28 +0900

    미로 맵 생성

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -1,27 +1,40 @@
-﻿#include <iostream>
+﻿#include "pch.h"
+#include <iostream>
 #include "List.h"
 #include "Vector.h"
+#include "Board.h"
+#include "Player.h"
+
+Board board;
+Player player;
 
 int main()
 {
+	::srand(static_cast<uint32>(time(nullptr))); // 랜덤 seed 값 설정
+
+	while (true)
 	{
-		// 양방향 연결리스트
-		List list;
+		// 입력
 
-		list.AddAtTail(1);
-		Node* node = list.AddAtTail(2);
-		// 이렇게 특정 노드의 위치를 기억할 때 중간 삽입,삭제가 빠르다고 할 수 있다.
-		list.AddAtTail(3);
+		// 로직
 
-		list.Print();
-
-		list.Remove(node);
-
-		list.Print();
+		// 렌더링
+		board.Render();
 	}
 
-	{
-		// Vector v;
-	}
+	/*
+	양방향 연결리스트
+	List list;
+
+	list.AddAtTail(1);
+	Node* node = list.AddAtTail(2);
+	// 이렇게 특정 노드의 위치를 기억할 때 중간 삽입,삭제가 빠르다고 할 수 있다.
+	list.AddAtTail(3);
+	list.Print();
+
+	list.Remove(node);
+	list.Print();
+	*/
+
 }
 
