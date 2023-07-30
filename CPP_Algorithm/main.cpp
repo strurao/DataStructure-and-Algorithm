62c9566d38143fa077861dd2305082d1668bd40c
strurao <strurao@gmail.com>
2023-07-30 11:25:39 +0900

    트리

--- CPP_Algorithm/main.cpp
+++ CPP_Algorithm/main.cpp
@@ -1,7 +1,104 @@
 #include <iostream>
 using namespace std;
+#include <vector>
+
+class Node
+{
+public:
+	Node(const char* data):data(data){}
+
+public:
+	const char* data;
+	vector<Node*> children;
+};
+
+Node* CreateTree()
+{
+	Node* root = new Node("R1 개발실");
+	{
+		Node* node = new Node("디자인");
+		root->children.push_back(node);
+		{
+			Node* leaf = new Node("전투");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("경제");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("스토리");
+			node->children.push_back(leaf);
+		}
+	}
+	{
+		Node* node = new Node("프로그래밍");
+		root->children.push_back(node);
+		{
+			Node* leaf = new Node("클라");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("서버");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("엔진");
+			node->children.push_back(leaf);
+		}
+	}
+	{
+		Node* node = new Node("아트");
+		root->children.push_back(node);
+		{
+			Node* leaf = new Node("배경");
+			node->children.push_back(leaf);
+		}
+		{
+			Node* leaf = new Node("캐릭터");
+			node->children.push_back(leaf);
+		}
+	}
+	return root;
+}
+
+// 깊이(depth) : 루트에서 어떤 노드에 도달하기 위해 거쳐야 하는 간선의 개수
+void PrintTree(Node* root, int depth = 0)
+{
+	for (int i = 0; i < depth; i++)
+		cout << "-";
+
+	cout << root->data << endl;
+
+	int size = root->children.size();
+	for (int i = 0; i < size; i++)
+	{
+		Node* node = root->children[i];
+		PrintTree(node, depth + 1);
+	}
+}
+
+// 높이
+int GetHeight(Node* root)
+{
+	int ret = 1;
+
+	int size = root->children.size();
+	for (int i = 0; i < size; i++)
+	{
+		Node* node = root->children[i];
+		int h = GetHeight(node) + 1;
+
+		if (ret < h)
+			ret = h;
+	}
+	return ret;
+}
 
 int main()
 {
-
+	Node* root = CreateTree();
+	PrintTree(root);
+	int h = GetHeight(root);
+	cout << "h: " << h << endl;
 }
