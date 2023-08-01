9d1a576021bf6560a3afa0aeaf9eb6c755a512b5
strurao <strurao@gmail.com>
2023-08-01 16:41:03 +0900

    그래프 기초

--- CPP_Algorithm/main.cpp
+++ CPP_Algorithm/main.cpp
@@ -104,7 +104,7 @@ int GetHeight(Node* root)
 /////////////// 우선순위 큐 /////////////////////
 ////////////////////////////////////////////////
 
-template<typename T>
+template<typename T, typename Predicate = less<T>>
 class PriorityQueue
 {
 public:
@@ -185,6 +185,124 @@ private:
 
 };
 
+////////////////////////////////////////////////
+///////////////// 그래프 ///////////////////////
+////////////////////////////////////////////////
+
+// 직관적이지만 잘 사용되지 않는 방식이다
+void CreateGraph_1()
+{
+	struct Vertex
+	{
+		// int data;
+		vector<Vertex*> edges; // 간선
+	};
+
+	vector<Vertex> v;
+	v.resize(6); // size
+	// v.reserve(6); // capacity
+
+	v[0].edges.push_back(&v[1]);
+	v[0].edges.push_back(&v[3]);
+	v[1].edges.push_back(&v[0]);
+	v[1].edges.push_back(&v[2]);
+	v[1].edges.push_back(&v[3]);
+	v[3].edges.push_back(&v[4]);
+	v[5].edges.push_back(&v[4]);
+
+	// Q) 0->3 연결되어있나요?
+	bool connected = false;
+
+	int size = v[0].edges.size();
+	for (int i = 0; i < size; i++)
+	{
+		Vertex* vertex = v[0].edges[i];
+		if (vertex == &v[3])
+		{
+			connected = true;
+		}
+	}
+}
+
+// 인접 리스트 : 실제 연결된 노드들'만' 넣어준다
+// 인접했다는 것은 곧 연결되어있다는 의미이다
+// 지하철 노선도처럼 서로 드문 드문 연결되어있을 때 적절하다
+void CreateGraph_2()
+{
+	struct Vertex
+	{
+		int data;
+	};
+
+	vector<Vertex> v(6); // 정점은 여기서 관리하고
+
+	vector<vector<int>> adjacent; // 연결 여부는 이중 벡터에서 관리한다
+	adjacent.resize(6);
+
+	// adjacent[0].push_back(1);
+	// adjacent[0].push_back(3);
+	adjacent[0] = { 1, 3 };
+	adjacent[1] = { 0, 2, 3 };
+	adjacent[3] = { 4 };
+	adjacent[5] = { 4 };
+
+	// Q) 0->3 연결되어있나요?
+	bool connected = false;
+
+	int size = adjacent[0].size();
+	for (int i = 0; i < size; i++)
+	{
+		int vertex = adjacent[0][i];
+		if (vertex == 3)
+		{
+			connected = true;
+		}
+	}
+}
+
+// 인접 행렬 
+// 페이스북처럼 서로 빽빽하게 연결되어 있는 경우에 적절하다
+void CreateGraph_3()
+{
+	struct Vertex
+	{
+		int data;
+	};
+
+	vector<Vertex> v(6);
+
+	// 연결된 목록을 행렬로 관리 (2차원 표처럼)
+	// [X][O][X][O][X][X]
+	// [O][X][O][O][X][X]
+	// [X][X][X][X][X][X]
+	// [X][X][X][X][O][X]
+	// [X][X][X][X][X][X]
+	// [X][X][X][X][O][X]
+
+	// adjacent[from][to] ?
+	// 행렬을 이용한 그래프 표현
+	// 메모리 소모 심하지만, 빠른 접근
+	vector<vector<bool>> adjacent(6, vector<bool>(6, false));
+	adjacent[0][1] = true;
+	adjacent[0][3] = true;
+	adjacent[1][0] = true;
+	adjacent[1][2] = true;
+	adjacent[3][4] = true;
+	adjacent[5][4] = true;
+
+	// Q) 0->3 연결되어있나요?
+	bool connected = adjacent[0][3];
+
+	// bool 대신 int로 구현한다면?
+	vector<vector<int>> adjacent2 =
+	{
+		{-1, 15, -1, 35, -1, -1},
+		{15, -1, +5, 10, -1, -1},
+		{-1, +5, -1, -1, -1, -1},
+		{-1, -1, -1, +5, -1, +5},
+		{-1, -1, -1, -1, +5, -1},
+	};
+}
 
 
 int main()
@@ -207,6 +325,15 @@ int main()
 	pq.push(20);
 	int value = pq.top();
 	pq.pop();
+	*/
+
+	
+	/*
+	결국 우리는 순회를 해야 한다 
+	그래프 순회는 어떻게 해야할까? 트리는 재귀, 벡터는 쭉 선형탐색하면 된다.
+	그래프는 트리처럼 계층적이지 않으므로 싸이클이 일어날 수 있다. 
+	규칙이 필요하다.
+	그래서 DFS, BFS 의 필요성이 대두된다.
 	*/
 
 }
