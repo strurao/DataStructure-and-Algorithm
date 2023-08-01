84e4ca1907c7e768298ce1c5be0a806fe5860d9a
strurao <strurao@gmail.com>
2023-08-01 16:02:58 +0900

    우선순위 큐

--- CPP_Algorithm/main.cpp
+++ CPP_Algorithm/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <queue>
+
+////////////////////////////////////////////////
+////////////////// 트리 ////////////////////////
+////////////////////////////////////////////////
 
 class Node
 {
@@ -95,10 +100,113 @@ int GetHeight(Node* root)
 	return ret;
 }
 
+////////////////////////////////////////////////
+/////////////// 우선순위 큐 /////////////////////
+////////////////////////////////////////////////
+
+template<typename T>
+class PriorityQueue
+{
+public:
+	// O(logN) 트리 높이에 의존적이므로
+	void push(const T& data)
+	{
+		// 우선 힙 구조부터 맞춰준다
+		_heap.push_back)back(data);
+
+		// 도장깨기 시작
+		int now = static_cast<int>(_haep.size()) - 1;
+
+		// 루트 노드까지
+		while (now > 0)
+		{
+			// 부모 노드와 비교해서 더 작으면 패배
+			int next = (now - 1) / 2; 
+
+			if (_heap[now] < _heap[next])
+				break;
+
+			// 데이터 교체
+			::swap(_heap[now], _heap[next]);
+			now = next;
+		}
+	}
+
+	// O(logN) 재배치 과정 또한 트리의 높이에 의존적이므로
+	void pop()
+	{
+		_heap[0] = _heap.back();
+		_heap.pop_back();
+
+		int now = 0;
+
+		while (true)
+		{
+			int left = 2 * now + 1;
+			int right = 2 * now + 2;
+
+			// 예외처리 : 리프에 도달한 경우 
+			if (left >= (int)_heap.size())
+				break;
+
+			int next = now;
+
+			// 왼쪽 비교
+			if (_heap[next] < _heap[left])
+				next = left;
+
+			// 둘 중 승자를 오른쪽과 비교
+			if (right < _heap.size() && _heap[next] < _heap[right])
+				next = right;
+
+			// 왼쪽/오른쪽 둘 다 현재 값보다 작으면 종료
+			if (next == now)
+				break;
+
+			::swap(_heap[now], _heap[next]);
+			now = next;
+		}
+	}
+
+	// O(1)
+	T& top()
+	{
+		return _heap[0];
+	}
+
+	// O(1)
+	bool empty()
+	{
+		return _heap.empty();
+	}
+
+private:
+	vector<T> _heap;
+
+};
+
+
+
 int main()
 {
+	/*
+	트리
 	Node* root = CreateTree();
 	PrintTree(root);
 	int h = GetHeight(root);
 	cout << "h: " << h << endl;
+	*/ 
+	
+	/*
+	우선순위 큐
+	PriorityQueue<int> pq;
+	pq.push(10);
+	pq.push(40);
+	pq.push(30);
+	pq.push(50);
+	pq.push(20);
+	int value = pq.top();
+	pq.pop();
+	*/
+
 }
