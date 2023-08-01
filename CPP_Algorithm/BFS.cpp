2a3364bdf2d419af47a4efdd6e212516a52e29ed
strurao <strurao@gmail.com>
2023-08-01 21:07:49 +0900

    BFS(1)

--- CPP_Algorithm/BFS.cpp
+++ CPP_Algorithm/BFS.cpp
@@ -8,23 +8,27 @@ struct Vertex
 
 };
 
-vector<Vertex> vertices;
-vector<vector<int>> adjacent;
-vector<vector<int>> adjacent2;
+vector<Vertex> vertices_bfs;
+vector<vector<int>> adjacent_bfs;
+vector<vector<int>> adjacent_bfs2;
 
-void CreateGraph()
+// 발견한
+vector<bool> discovered;
+
+void CreateGraph_bfs()
 {
-	vertices.resize(6);
+	vertices_bfs.resize(6);
 
 	// 인접 리스트
-	adjacent = vector<vector<int>>(6);
-	adjacent[0] = { 1, 3 };
-	adjacent[1] = { 0, 2, 3 };
-	adjacent[3] = { 4 };
-	adjacent[5] = { 4 };
-
+	adjacent_bfs = vector<vector<int>>(6);
+	adjacent_bfs[0] = { 1, 3 };
+	adjacent_bfs[1] = { 0, 2, 3 };
+	adjacent_bfs[3] = { 4 };
+	adjacent_bfs[5] = { 4 };
+	
 	// 인접 행렬
-	adjacent2 = vector<vector<int>>
+	/*
+	adjacent_bfs = vector<vector<int>>
 	{
 		{ 0, 1, 0, 1, 0, 0 },
 		{ 1, 0, 1, 1, 0, 0 },
@@ -33,16 +37,43 @@ void CreateGraph()
 		{ 0, 0, 0, 0, 0, 0 },
 		{ 0, 0, 0, 0, 1, 0 }
 	};
-
+	*/
 }
 
+/*
+발견한 순서대로 접근하겠다 -> 선입선출 큐
+*/
 void Bfs(int here) // 첫 호출 시 here 에 시작점이 들어간다
 {
+	queue<int> q;
+	q.push(here);
+	discovered[here] = true;
+	
+	while (q.empty() == false)
+	{
+		here = q.front();
+		q.pop();
 
+		// 방문 도장
+		cout << "BFS Visited : " << here << endl;
+
+		int size = adjacent_bfs[here].size();
+		for (int i = 0; i < size; i++)
+		{
+			int there = adjacent_bfs[here][i];
+			if (discovered[there])
+				continue;
+		
+			q.push(there);
+			discovered[there] = true;
+		}
+	}
 }
 
 int main()
 {
-
+	CreateGraph_bfs();
+	discovered = vector<bool>(6, false);
+	Bfs(0);
 }
 
